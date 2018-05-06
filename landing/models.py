6fc57c83869fa2b14e95e10de67901172bf7ac85
YECHEZ <nikitafedotov2010@yandex.ru>
2018-05-06 13:02:50 +0300

    Создана модель Подписчиков для лэндинга

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Subscriber(models.Model):
+    email = models.EmailField(verbose_name='E-mail')
+    name = models.CharField(verbose_name='Имя', max_length=128)
+    is_active = models.BooleanField(verbose_name='Рассылка', default=True)
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+
+    def __str__(self):
+        return "Рассылка для %s (%s)" % (self.email, self.name)
