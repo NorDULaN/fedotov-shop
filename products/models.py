99767aa7789585d636990b9742d63e7085601057
YECHEZ <nikitafedotov2010@yandex.ru>
2018-05-09 14:45:27 +0300

    Обновлены шаблоны; исправлена логика отображения продуктов

--- products/models.py
+++ products/models.py
@@ -55,3 +55,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolut_url(self):
+        slugs = []
+        slugs = self.category.get_slug_list()
+        last = slugs[-1]
+        return last + '/' + self.slug      
--- products/views.py
+++ products/views.py
@@ -5,33 +5,25 @@ from .models import Category, Product
 
 # Страница с товарами
 def ProductList(request):
-    root = Category.objects.all()
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {
-        'category': category,
+    categories = Category.objects.filter(parent=None)
+    return render(request, 'products/index.html', {
         'categories': categories,
-        'products': products
     })
 
 def show_category(request,hierarchy= None):
     category_slug = hierarchy.split('/')
     parent = None
     root = Category.objects.all()
-    print(root)
 
     for slug in category_slug[:-1]:
         parent = root.get(parent=parent, slug = slug)
-        print(parent)
 
     try:
         instance = Category.objects.get(parent=parent,slug=category_slug[-1])
     except:
         instance = get_object_or_404(Product, slug = category_slug[-1])
-        return render(request, "products/productDetail.html", {'instance':instance})
+        return render(request, "products/productDetail.html", {
+            'instance':instance,
+            })
     else:
-        return render(request, 'products/products.html', {'instance':instance})
+        return render(request, 'products/productInCats.html', {'instance':instance})
